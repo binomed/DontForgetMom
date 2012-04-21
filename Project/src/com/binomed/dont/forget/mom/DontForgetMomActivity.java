de082fdc1d83d5ab6e78c12cb69f0ac355bc0e5f
jefBinomed <@ndroid44>
2012-04-21 16:28:36 +0200

    First Commit

---
+++ Project/src/com/binomed/dont/forget/mom/DontForgetMomActivity.java
@@ -0,0 +1,13 @@
+package com.binomed.dont.forget.mom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DontForgetMomActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
