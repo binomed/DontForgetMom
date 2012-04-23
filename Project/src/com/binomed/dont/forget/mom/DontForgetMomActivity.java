0774f639e16ba67833198f1a2c28abdf9f5a4b0f
jefBinomed <jean.francois.garreau@gmail.com>
2012-04-23 14:05:17 +0200

    Commit du premier écran

--- Project/src/com/binomed/dont/forget/mom/DontForgetMomActivity.java
+++ Project/src/com/binomed/dont/forget/mom/DontForgetMomActivity.java
@@ -1,13 +1,14 @@
 package com.binomed.dont.forget.mom;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class DontForgetMomActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class DontForgetMomActivity extends SherlockActivity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
 }
