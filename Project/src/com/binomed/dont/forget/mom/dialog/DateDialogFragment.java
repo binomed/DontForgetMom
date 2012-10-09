f1d31e008903a251bf5c0dbbd00309774ac9b242
jefBinomed <@ndroid44>
2012-10-09 23:25:28 +0200

    mise en place fragments date et time picker

---
+++ Project/src/com/binomed/dont/forget/mom/dialog/DateDialogFragment.java
@@ -0,0 +1,31 @@
+package com.binomed.dont.forget.mom.dialog;
+
+import java.util.Calendar;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+public class DateDialogFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// Use the current time as the default values for the picker
+		final Calendar c = Calendar.getInstance();
+		int year = c.get(Calendar.YEAR);
+		int month = c.get(Calendar.MONTH);
+		int day = c.get(Calendar.DAY_OF_MONTH);
+
+		// Create a new instance of TimePickerDialog and return it
+		return new DatePickerDialog(getActivity(), this, year, month, day);
+	}
+
+	@Override
+	public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+		// Do something with the time chosen by the user
+
+	}
+
+}
--- Project/src/com/binomed/dont/forget/mom/screen/edit/EditionActivity.java
+++ Project/src/com/binomed/dont/forget/mom/screen/edit/EditionActivity.java
@@ -1,12 +1,53 @@
 package com.binomed.dont.forget.mom.screen.edit;
 
+import roboguice.inject.InjectView;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 
 import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.binomed.dont.forget.mom.R;
+import com.binomed.dont.forget.mom.dialog.DateDialogFragment;
+import com.binomed.dont.forget.mom.dialog.TimeDialogFragment;
 import com.binomed.dont.forget.mom.utils.AbstractActivity;
 
 public class EditionActivity extends AbstractActivity {
+
+	public static final int ITEM_SAVE = 3;
+
+	@InjectView(R.id.tripNameTxt)
+	EditText tripNameTxt;
+	@InjectView(R.id.placeEdit)
+	EditText placeEdit;
+	@InjectView(R.id.btnMap)
+	ImageButton btnMap;
+	@InjectView(R.id.btnContactPlace)
+	ImageButton btnContactPlace;
+	@InjectView(R.id.btnDate)
+	Button btnDate;
+	@InjectView(R.id.btnHour)
+	Button btnHour;
+	@InjectView(R.id.departHour)
+	CheckBox departHour;
+	@InjectView(R.id.seekPrecision)
+	SeekBar seekPrecision;
+	@InjectView(R.id.radioGroupAlert)
+	RadioGroup radioGroupAlert;
+	@InjectView(R.id.btContactRecipient)
+	ImageButton btContactRecipient;
+	@InjectView(R.id.recipients)
+	EditText recipients;
+	@InjectView(R.id.messageContent)
+	EditText messageContent;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -16,6 +57,59 @@ public class EditionActivity extends AbstractActivity {
 
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		menu.add(0, ITEM_SAVE, 1, R.string.action_save) //
+				.setIcon(R.drawable.ic_action_save) //
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		super.onCreateOptionsMenu(menu);
+
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+
+		switch (item.getItemId()) {
+		case ITEM_SAVE:
+			Intent intent = new Intent(this, EditionActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+			startActivity(intent);
+			return true;
+
+		default:
+			return super.onMenuItemSelected(featureId, item);
+		}
+
+	}
+
+	public void onClick(final View view) {
+		switch (view.getId()) {
+		case R.id.btnMap:
+			break;
+		case R.id.btnContactPlace:
+			break;
+		case R.id.btnDate:
+			DialogFragment dateFragment = new DateDialogFragment();
+			dateFragment.show(getSupportFragmentManager(), "datePicker");
+			break;
+		case R.id.btnHour:
+			DialogFragment hourFragment = new TimeDialogFragment();
+			hourFragment.show(getSupportFragmentManager(), "timePicker");
+			break;
+		case R.id.departHour:
+			btnHour.setEnabled(departHour.isChecked());
+			break;
+		case R.id.btContactRecipient:
+
+			break;
+
+		default:
+			break;
+		}
 	}
 
 }
