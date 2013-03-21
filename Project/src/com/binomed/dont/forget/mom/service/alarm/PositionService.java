78ae235d3354eb4dea06b5fa790880b9b6cc6c07
jefBinomed <@ndroid44>
2013-03-22 00:47:47 +0100

    debut mise en place notifications

--- Project/src/com/binomed/dont/forget/mom/service/alarm/PositionService.java
+++ Project/src/com/binomed/dont/forget/mom/service/alarm/PositionService.java
@@ -2,6 +2,15 @@ package com.binomed.dont.forget.mom.service.alarm;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.binomed.dont.forget.mom.R;
+import com.binomed.dont.forget.mom.db.DontForgetMomContentProvider;
+import com.binomed.dont.forget.mom.db.DontForgetMomDbInformation.Trip;
 
 public class PositionService extends IntentService {
 
@@ -11,7 +20,34 @@ public class PositionService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		// TODO Auto-generated method stub
+
+		Cursor cursor = getContentResolver().query(DontForgetMomContentProvider.CONTENT_URI //
+				, null // projection
+				, Trip._ID + "=?" // selection
+				, new String[] { String.valueOf(intent.getLongExtra(Trip._ID, -1L)) } // args
+				, null // sort
+				);
+		if (cursor.moveToFirst()) {
+
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+			if (prefs.getBoolean(getString(R.string.pref_automatic_send_key), true)) {
+				// Do action
+			} else {
+				// Notification send alert
+				if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+
+				} else {
+
+				}
+			}
+
+		}
+
+		try {
+			cursor.close();
+		} catch (Exception e) {
+			Log.e(PositionService.class.getName(), e.getMessage(), e);
+		}
 
 	}
 
